76df725da4c9e5086f58e8f9b66fc0b34347824d
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-04-29 02:11:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import user from './Profile/user.json';
+import data from './Statistic/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistic/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}  
+      />
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={ transactions} />
+    </>
+  )
 };
