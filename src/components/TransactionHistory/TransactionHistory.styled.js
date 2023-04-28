d1ab3995e4c719c62b44df896d1f2ab7e1ccbd69
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-04-29 02:11:50 +0300

    Create TransactionHistory.styled.js

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  margin: 0 auto;
+  width: 400px;
+  text-align: center;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2);
+    margin-bottom: 40px;
+`;
+
+export const Thead = styled.thead`
+background-color: aquamarine;
+`
+
+export const TableRow = styled.tr`
+  background-color: lightgrey;
+`;
